b48ba1d7568fcf94cb57d610def9cfdd21247c2f
Daniel Pacak <pacak.daniel@gmail.com>
2012-05-01 00:16:28 +0200

    Putting things together

---
+++ webconsole-ui/src/main/java/org/danielsoft/webconsole/ui/Activator.java
@@ -0,0 +1,18 @@
+package org.danielsoft.webconsole.ui;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
