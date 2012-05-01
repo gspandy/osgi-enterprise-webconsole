ae308d4ba1973bf0edac906b02e7599792b548fd
Daniel Pacak <pacak.daniel@gmail.com>
2012-05-01 20:34:32 +0200

    Removing temporal cohesion

--- webconsole-extjs-wrapper/src/main/java/org/danielsoft/webconsole/extjswrapper/Activator.java
+++ webconsole-extjs-wrapper/src/main/java/org/danielsoft/webconsole/extjswrapper/Activator.java
@@ -2,24 +2,47 @@ package org.danielsoft.webconsole.extjswrapper;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator, ServiceListener {
 
+	private BundleContext bundleContext;
 	private HttpService httpService;
 
 	public void start(BundleContext bundleContext) throws Exception {
-		ServiceReference ref = bundleContext
-				.getServiceReference(HttpService.class.getName());
+		this.bundleContext = bundleContext;
+		String httpServiceFilter = String.format("(objectClass=%s)", HttpService.class.getName());
+		ServiceReference[] httpServiceRefs = bundleContext.getServiceReferences(null, httpServiceFilter);
 
-		httpService = (HttpService) bundleContext.getService(ref);
-		httpService.registerResources("/extjs/4.1.0", "/web/extjs", null);
+		if (httpServiceRefs != null) {
+			registerResources(httpServiceRefs[0]);
+		} else {
+			bundleContext.addServiceListener(this, httpServiceFilter);
+		}
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		if (httpService != null) {
 			httpService.unregister("/extjs/4.1.0");
+		}
+	}
+	
+	public void serviceChanged(ServiceEvent event) {
+		if (event.getType() == ServiceEvent.REGISTERED) {
+			registerResources(event.getServiceReference());
+		}
+	}
+	
+	void registerResources(ServiceReference httpServiceRef) {
+		try {
+			httpService = (HttpService) bundleContext.getService(httpServiceRef);
+			httpService.registerResources("/extjs/4.1.0", "/web/extjs", null);
+		} catch (NamespaceException e) {
+			System.err.println("Error while registering resources: " + e);
 		}
 	}
 
--- webconsole-ui/src/main/java/org/danielsoft/webconsole/ui/Activator.java
+++ webconsole-ui/src/main/java/org/danielsoft/webconsole/ui/Activator.java
@@ -6,34 +6,59 @@ import org.danielsoft.webconsole.service.BundleServlet;
 import org.danielsoft.webconsole.service.BundleTreeServlet;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpService;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator, ServiceListener {
 
+	private BundleContext bundleContext;
 	private HttpService httpService;
 
 	public void start(BundleContext bundleContext) throws Exception {
-		ServiceReference ref = bundleContext
-				.getServiceReference(HttpService.class.getName());
-
-		httpService = (HttpService) bundleContext.getService(ref);
-		httpService.registerResources("/webconsole", "/web", null);
+		this.bundleContext = bundleContext;
+		String httpServiceFilter = String.format("(objectClass=%s)", HttpService.class.getName());
+		ServiceReference[] httpServiceRefs = bundleContext.getServiceReferences(null, httpServiceFilter);
 		
-		BundleServlet bundleServlet = new BundleServlet(bundleContext);
-		BundleTreeServlet bundleTreeServlet = new BundleTreeServlet(bundleContext);
-		BundleReadServlet bundleReadServlet = new BundleReadServlet(bundleContext);
-		BundleInstallServlet bundleInstallServlet = new BundleInstallServlet(bundleContext);
-		httpService.registerServlet("/webconsole/service/bundles", bundleServlet, null, null);
-		httpService.registerServlet("/webconsole/service/bundles/tree", bundleTreeServlet, null, null);
-		httpService.registerServlet("/webconsole/service/bundles/read", bundleReadServlet, null, null);
-		httpService.registerServlet("/webconsole/service/bundles/install", bundleInstallServlet, null, null);
+		if (httpServiceRefs != null) {
+			registerServletsAndResources(httpServiceRefs[0]);
+		} else {
+			bundleContext.addServiceListener(this, httpServiceFilter);
+		}
+
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		if (httpService != null) {
 			httpService.unregister("/webconsole");
 		}
+		bundleContext.removeServiceListener(this);
+	}
+	
+	public void serviceChanged(ServiceEvent event) {
+		if (event.getType() == ServiceEvent.REGISTERED) {
+			registerServletsAndResources(event.getServiceReference());
+		}
+	}
+	
+	void registerServletsAndResources(ServiceReference httpServiceRef) {
+		try {
+			httpService = (HttpService) bundleContext.getService(httpServiceRef);
+			httpService.registerResources("/webconsole", "/web", null);
+			
+			BundleServlet bundleServlet = new BundleServlet(bundleContext);
+			BundleTreeServlet bundleTreeServlet = new BundleTreeServlet(bundleContext);
+			BundleReadServlet bundleReadServlet = new BundleReadServlet(bundleContext);
+			BundleInstallServlet bundleInstallServlet = new BundleInstallServlet(bundleContext);
+			httpService.registerServlet("/webconsole/service/bundles", bundleServlet, null, null);
+			httpService.registerServlet("/webconsole/service/bundles/tree", bundleTreeServlet, null, null);
+			httpService.registerServlet("/webconsole/service/bundles/read", bundleReadServlet, null, null);
+			httpService.registerServlet("/webconsole/service/bundles/install", bundleInstallServlet, null, null);
+		} catch (Exception e) {
+			System.err.println("Error while registering resources and servlets: " + e);
+		}
+
 	}
 
 }
